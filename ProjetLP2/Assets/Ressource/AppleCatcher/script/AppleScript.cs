ecd27113a34b18f092ac9c01ce1ee9429e9b1e7f
Albtros26 <gratia.alban@gmail.com>
2026-05-30 12:53:25 +0200

    création du projet
    je viens de créer le dossier du projet mais il est trop lourd pour être envoyé en une fois donc il va avoir plien de commit

---
+++ ProjetLP2/Assets/Ressource/AppleCatcher/script/AppleScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AppleScript : MonoBehaviour
+{
+    void Update()
+    {
+        if (transform.position.y < -10f)
+        {
+            if (SceneManager.GetActiveScene().name == "game")
+            {
+                AppleSpawner.Instance.LoseHealth();
+            }
+
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Destroy(gameObject);
+    }
+}
