346caef6798c73047d6c26c243d7dd35fa71b41b
Fowyous <mohamed.koubaa@utbm.fr>
2026-05-30 20:51:12 +0200

    added enemy spawner code(unfinished)

---
+++ ProjetLP2/Assets/Ressource/shooter/Code/EnemySpawner.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections.Generic;
+using static ShooterConstants;
+
+public class EnemySpawner : MonoBehaviour
+{
+  //this is the list of enemies that will be added through the unity editor
+  [SerializeField] private List<GameObject> enemiesPrefabs = new List<GameObject>();
+
+  /// <summary>
+  /// Loads a random enemy.
+  /// </summary>
+  private void LoadRandomEnemy()
+  {
+
+    GameObject newenemy = Instantiate(enemiesPrefabs[Random.Range(0, enemiesPrefabs.Count)]);
+    newenemy.transform.position = new Vector3(10.5f, Random.Range(-ShooterConstants.GameLimit.y, ShooterConstants.GameLimit.y));
+  }
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
+  {
+    LoadRandomEnemy();
+
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
---
+++ ProjetLP2/Assets/Ressource/shooter/Code/ShooterConstants.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public static class ShooterConstants
+{
+  public const string AppName = "MyApplication";
+  public static class GameLimit
+  {
+    public const float x = 8.2f;
+    public const float y = 4.5f;
+  }
+}
